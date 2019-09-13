3c55a13b9a61b81eeec6c0d6e0b59abc6b3b10d9
Perth <perth.ploy@gmail.com>
2019-09-13 16:55:46 +0700

    Finished Battle class

--- src/logic/Battle.java
+++ src/logic/Battle.java
@@ -2,7 +2,7 @@ package logic;
 
 public class Battle {
 	
-	private Hero fighter[];
+	private Hero[] fighter = new Hero[2];
 	
 	public Battle(Hero a, Hero b) {
 		fighter[0] = a;
@@ -13,34 +13,46 @@ public class Battle {
 		System.out.println("==== Here Come A New Challenger ====");
 		// TODO Show opening phrase -> Show challengers -> Repeat doBattleStep() -> Show result
 		System.out.println("== " + fighter[0].getName() + " " + fighter[0].printStat() + " VS " + fighter[1].getName() + " " + fighter[1].printStat() + " ==\n");
+		while(!isEnd()) {
+			doBattleStep();
+		}
+		showResult();
 	}
 
 	public void doBattleStep() {
 		// TODO FILL ME
 		// First fighter Attack
-		System.out.println(/* ATTACKER NAME */ + " do " + /* FILL DAMAGE HERE */ + " damage to " + /* TARGET NAME */);
-		System.out.println(/* TARGET NAME */ + " has " + /* TARGET HP */ + " HP left\n");
+		System.out.println(fighter[0].getName() + " do " + fighter[0].doDamage(fighter[1]) + " damage to " + fighter[1].getName());
+		System.out.println(fighter[1].getName() + " has " + fighter[1].getHp() + " HP left\n");
 		// Second fighter Attack
-		System.out.println(/* ATTACKER NAME */ + " do " + /* FILL DAMAGE HERE */ + " damage to " + /* TARGET NAME */);
-		System.out.println(/* TARGET NAME */ + " has " + /* TARGET HP */ + " HP left\n");
-		if(/* CHECK IF FIRST FIGHTER IS DEFEATED */) {
-			System.out.println(/* FILL NAME */ + " is knocked out\n");
+		System.out.println(fighter[1].getName() + " do " + fighter[1].doDamage(fighter[0]) + " damage to " + fighter[0].getName());
+		System.out.println(fighter[0].getName() + " has " + fighter[0].getHp() + " HP left\n");
+		if(fighter[0].isDefeated()) {
+			System.out.println(fighter[0].getName() + " is knocked out\n");
 		}
-		if(/* CHECK IF SECOND FIGHTER IS DEFEATED */) {
-			System.out.println(/* FILL NAME */ + " is knocked out\n");
+		if(fighter[1].isDefeated()) {
+			System.out.println(fighter[1].getName() + " is knocked out\n");
+		}
+	}
+	
+	public boolean isEnd() {
+		if(fighter[0].isDefeated() || fighter[1].isDefeated()) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
 	// TODO Fill me
 	public void showResult() {
-		if(/* FILL ME */) {
+		if(fighter[0].isDefeated() && fighter[1].isDefeated()) {
 			System.out.println("== DRAW ==");
 		}
-		else if(/* FILL ME */) {
-			System.out.println("== " + /* Winner's Name */ + " WIN ==");
+		else if(fighter[0].isDefeated() && !fighter[1].isDefeated()) {
+			System.out.println("== " + fighter[1].getName() + " WIN ==");
 		}
-		else if(/* FILL ME */) {
-			System.out.println("== " + /* Winner's Name */ + " WIN ==");
+		else if(!fighter[0].isDefeated() && fighter[1].isDefeated()) {
+			System.out.println("== " + fighter[0].getName() + " WIN ==");
 		}
 	}
 }
